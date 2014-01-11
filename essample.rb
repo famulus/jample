d932eaf3bd11dd85b991735e61d95789afa07d77
famulus <famulus.fusion@gmail.com>
2014-01-11 15:53:20 -0500

    wip

--- essample.rb
+++ essample.rb
@@ -1,34 +1,38 @@
 require 'mustache'
 
 
-# afconvert --file WAVE --data LEI16 /Users/clean/Downloads/take_5.m4a
 
-# `aubiocut -i /Users/clean/Downloads/take_5.wav  -Lc`
+project_root = "/Users/clean/Documents/essample/"
 
-
-hydrogen = File.read("hydrogen.mustache")
-
-# home_keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?"
-
-# home_keys_array = home_keys.bytes.to_a
+raw_songs = File.join(project_root, 'raw_songs')
+slices_folder = File.join(project_root, 'slices')
+hydrogen = File.read(File.join(project_root, "hydrogen.mustache"))
 
 
-home = "/Users/clean/Desktop/slices/"
-
-files = []
-
-Dir.foreach(home) do |item|
-	next if item == '.' or item == '..' 
-	next unless item.include?("wav")
-	# do work on real items
-	files << item
+# For each song in raw songs
+Dir.foreach(raw_songs) do |raw_song|
+	next if raw_song == '.' or raw_song == '..' 
+	next unless raw_song.include?("mp3")
+	puts str = "aubiocut -i #{File.join(raw_songs, raw_song)} -c -b"
+	puts `#{str}`
 end
 
-puts files
-
-channels = files.each_with_index.map { |f,i| {file_path: "#{f}", channel_index:i }}
-
-new_hydrogen =  Mustache.render(hydrogen, {channels: channels, channel_count: channels.size}  )
 
 
-File.write("#{home}new_hydrogen.h2song",new_hydrogen )
+
+# files = []
+# Dir.foreach(slices_folder) do |item|
+# 	next if item == '.' or item == '..' 
+# 	next unless item.include?("wav")
+# 	# do work on real items
+# 	files << item
+# end
+
+# puts files
+
+# channels = files.each_with_index.map { |f,i| {file_path: "#{f}", channel_index:i }}
+
+# new_hydrogen =  Mustache.render(hydrogen, {channels: channels, channel_count: channels.size}  )
+
+
+# File.write("#{slices_folder}new_hydrogen.h2song",new_hydrogen )
