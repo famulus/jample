56c488674da1cd6ba794abc272171b2f3b886ea7
famulus <famulus.fusion@gmail.com>
2014-01-11 11:29:20 -0500

    update h2 template

--- essample.rb
+++ essample.rb
@@ -6,11 +6,11 @@ require 'mustache'
 # `aubiocut -i /Users/clean/Downloads/take_5.wav  -Lc`
 
 
-giada = File.read("giada.mustache")
+hydrogen = File.read("hydrogen.mustache")
 
-home_keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?"
+# home_keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?"
 
-home_keys_array = home_keys.bytes.to_a
+# home_keys_array = home_keys.bytes.to_a
 
 
 home = "/Users/clean/Desktop/slices/"
@@ -18,18 +18,17 @@ home = "/Users/clean/Desktop/slices/"
 files = []
 
 Dir.foreach(home) do |item|
-  next if item == '.' or item == '..' 
-  next unless item.include?("wav")
-  # do work on real items
-files << item
-
+	next if item == '.' or item == '..' 
+	next unless item.include?("wav")
+	# do work on real items
+	files << item
 end
 
 puts files
 
-channels = files.each_with_index.map { |f,i| {file_path: "#{f}", channel_index:i, channel_letter: (home_keys_array[i]) }}
+channels = files.each_with_index.map { |f,i| {file_path: "#{f}", channel_index:i }}
 
-new_giada =  Mustache.render(giada, {channels: channels, channel_count: channels.size}  )
+new_hydrogen =  Mustache.render(hydrogen, {channels: channels, channel_count: channels.size}  )
 
 
-File.write("#{home}new_giada.gptc",new_giada )
+File.write("#{home}new_hydrogen.h2song",new_hydrogen )
