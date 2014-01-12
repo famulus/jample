6d12e9de52ae4a2c79950703c93c18c639037b55
famulus <famulus.fusion@gmail.com>
2014-01-12 10:21:28 -0500

    update aubiocut call

--- essample.rb
+++ essample.rb
@@ -10,6 +10,8 @@ slices_folder = File.join(project_root, 'slices')
 hydrogen = File.read(File.join(project_root, "hydrogen.mustache"))
 
 
+FileUtils.rm Dir.glob(File.join(slices_folder,"*"))
+
 # For each song in raw songs
 Dir.foreach(raw_songs) do |raw_song|
 	next if raw_song == '.' or raw_song == '..' 
@@ -21,19 +23,19 @@ end
 
 
 
-# files = []
-# Dir.foreach(slices_folder) do |item|
-# 	next if item == '.' or item == '..' 
-# 	next unless item.include?("wav")
-# 	# do work on real items
-# 	files << item
-# end
+files = []
+Dir.foreach(slices_folder) do |item|
+	next if item == '.' or item == '..' 
+	next unless item.include?("wav")
+	# do work on real items
+	files << item
+end
 
-# puts files
+puts files
 
-# channels = files.each_with_index.map { |f,i| {file_path: "#{f}", channel_index:i }}
+channels = files.each_with_index.map { |f,i| {file_path: "#{f}", channel_index:i }}
 
-# new_hydrogen =  Mustache.render(hydrogen, {channels: channels, channel_count: channels.size}  )
+new_hydrogen =  Mustache.render(hydrogen, {channels: channels, channel_count: channels.size}  )
 
 
-# File.write("#{slices_folder}new_hydrogen.h2song",new_hydrogen )
+File.write("#{slices_folder}new_hydrogen.h2song",new_hydrogen )
