55d44a05060dd90b24258e108d644c23bcb9e392
famulus <famulus.fusion@gmail.com>
2014-02-16 14:54:08 -0500

    latest essample

--- essample.rb
+++ essample.rb
@@ -2,23 +2,30 @@ require 'mustache'
 require 'fileutils'
 
 
-
 project_root = "/Users/clean/Documents/essample/"
 
 raw_songs = File.join(project_root, 'raw_songs')
 slices_folder = File.join(project_root, 'slices')
 hydrogen = File.read(File.join(project_root, "hydrogen.mustache"))
 
+puts raw_songs
 
-# FileUtils.rm Dir.glob(File.join(slices_folder,"*"))
+FileUtils.rm Dir.glob(File.join(slices_folder,"*"))
 
-# # For each song in raw songs
-# Dir.foreach(raw_songs) do |raw_song|
-# 	next if raw_song == '.' or raw_song == '..' 
-# 	next unless raw_song.include?("mp3")
-# 	puts str = "aubiocut -i \"#{File.join(raw_songs, raw_song)}\" -c  --cut-until-nslices=10 -o #{slices_folder}"
-# 	puts `#{str}`
-# end
+# For each song in raw songs
+Dir.foreach(raw_songs) do |raw_song|
+	next if raw_song == '.' or raw_song == '..' 
+	next unless raw_song.include?("mp3") or raw_song.include?("m4a")
+	puts str = "aubiocut -i \"#{File.join(raw_songs, raw_song)}\" -c  --cut-until-nslices=10 -o #{slices_folder}"
+	puts `#{str}`
+end
+
+Dir.glob(File.join(slices_folder,"*")).each do |original_file|
+	next if original_file == '.' or original_file == '..' or original_file == '.DS_Store' 
+	underscore_file = original_file.gsub(" ","_")
+	FileUtils.mv(original_file,underscore_file) rescue nil
+	puts "Renamed:  #{original_file} =&gt; #{underscore_file}"
+end
 
 
 
@@ -37,7 +44,7 @@ start_at = 0
 
 pure_data_list = files.map{|f| "#{File.join(slices_folder, f)};"}.join("\n")
 
-File.write(File.join(project_root,'pure_data','pd_list.txt'),pure_data_list )
+File.write(File.join(project_root,'pure_data','tmp','pd_list.txt'),pure_data_list )
 
 puts files = files[(start_at..(start_at+100))]
 
