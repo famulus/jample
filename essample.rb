3b69edeb324375cab316525332262b8f34e88553
famulus <famulus.fusion@gmail.com>
2014-02-22 11:15:22 -0500

    add support for wav

--- essample.rb
+++ essample.rb
@@ -11,18 +11,19 @@ puts "About to process #{raw_songs}"
 
 # FileUtils.rm Dir.glob(File.join(slices_folder,"*"))
 
+
 # Run aubiocut on each song in raw songs
 song_index = []
-Dir.mkdir(metadata_folder)
+Dir.mkdir(metadata_folder) rescue nil
 Dir.foreach(raw_songs) do |raw_song|
 	next if raw_song == '.' or raw_song == '..' 
-	next unless raw_song.include?("mp3") or raw_song.include?("m4a")
+	next unless raw_song.include?("mp3") or raw_song.include?("m4a") or raw_song.include?("wav")
 	underscore_file = raw_song.gsub(" ","_").gsub('(','').gsub(')','')
 	underscore_file_no_ext = File.basename(underscore_file, ".*")
 	current_song_dir = (File.join(slices_folder,underscore_file_no_ext ))
 	Dir.mkdir(current_song_dir) rescue nil
-	puts str = "aubiocut -i \"#{File.join(raw_songs, raw_song)}\" -c  --cut-until-nslices=15 -o #{current_song_dir}"
-	puts `#{str}`
+	puts aubiocut_command = "aubiocut -i \"#{File.join(raw_songs, raw_song)}\" -c  --cut-until-nslices=15 -o #{current_song_dir}"
+	puts `#{aubiocut_command}`
 	# convert spaces to underscore in slice filenames
 	Dir.glob(File.join(current_song_dir,"*")).each do |original_file|
 		next if original_file == '.' or original_file == '..' or original_file == '.DS_Store' 
