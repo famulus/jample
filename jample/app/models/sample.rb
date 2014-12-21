c57be1ce2e102959f277cdde82d44f901a5ca358
famulus <famulus.fusion@gmail.com>
2014-12-21 14:16:38 -0500

    function to convert onset index to time

--- jample/app/models/sample.rb
+++ jample/app/models/sample.rb
@@ -1,3 +1,6 @@
+include TimeHelpers
+
+
 class Sample
   include Mongoid::Document
 
@@ -5,8 +8,24 @@ class Sample
   field :track_id, type: String
   field :start_onset_index, type: Integer
   field :stop_onset_index, type: Integer
+  field :duration, type: Float
 
   belongs_to :track
+
+
+  # what makes a sample unique? 
+
+
+  def start_onset_time
+    return nil unless self.start_onset_index.present? && self.track.present? && self.track.onset_times.present?
+    start_onset_time = self.track.onset_times[self.start_onset_index]
+  end
+
+  def stop_onset_time
+    return nil unless self.stop_onset_index.present? && self.track.present? && self.track.onset_times.present?
+    stop_onset_time = self.track.onset_times[self.stop_onset_index]
+  end
+
 
 
 
