b7e4231bb9c5ec856f165d98584e48e7542d42b2
famulus <famulus.fusion@gmail.com>
2016-01-30 00:06:31 -0500

    clean up

--- jample/app/models/track.rb
+++ jample/app/models/track.rb
@@ -22,24 +22,23 @@ class Track
     track_list__wav_string = ''# `find ~/  -name *.wav`
     tracks_array = track_list_string.split("\n")
     tracks_array =  tracks_array + track_list__wav_string.split("\n")
-    puts tracks_array
-    puts tracks_array.size
-    sleep 7
+    # puts tracks_array
+    puts "#{tracks_array.size} tracks to check"
 
     # remove Track records when there is no underlying file
     missing_files = Track.nin(path_and_file: tracks_array)
     missing_files.each  do |missing_file|
       missing_file.track_missing = true
       missing_file.save
+      puts "Deleted #{missing_file}"
     end
 
     tracks_array.each do |track_path|
-      track_path
       next if track_path.include?('pure_data/tmp/patch') # don't ingest the temporary files
       file_contents_hash = Digest::MD5.file(track_path).hexdigest # hash the file contents, like a fingerprint
       track = Track.where(file_contents_hash: file_contents_hash).first_or_initialize
       if track.new_record?
-        puts "track_path:#{track_path}"
+        puts "track_path:#{track_path.to_s}"
         track.path_and_file = track_path
         track.mp3_data
         track.detect_onset
@@ -51,7 +50,6 @@ class Track
 
   def number_of_slices
     self.onset_count
-    
   end
 
 
