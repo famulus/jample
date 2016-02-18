96d5a2b8fc1102847ac54dc96eee9f8d44d2dafd
famulus <famulus.fusion@gmail.com>
2016-02-17 19:23:32 -0500

    no hardcoded patch directory

--- jample/app/models/concerns/settings.rb
+++ jample/app/models/concerns/settings.rb
@@ -1,5 +1,7 @@
 module Settings
 
-      PATCH_DIRECTORY = "/Users/clean/Documents/essample/pure_data/tmp/patch"
+      # PATCH_DIRECTORY = "/Users/clean/Documents/essample/pure_data/tmp/patch"
+      PATCH_DIRECTORY = Rails.root.join('../', 'pure_data', 'tmp', 'patch')
+
 
 end
