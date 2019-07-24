2a7688f8e64f968f9a94de9b21d415a5a904f09e
famulus <famulus.fusion@gmail.com>
2019-07-24 12:15:40 -0400

    new function for voicers

--- jample/app/models/patch.rb
+++ jample/app/models/patch.rb
@@ -36,6 +36,68 @@
 
 
 
+    def randomize_voice(subset_of_track_ids)
+
+      # return if self.is_frozen?
+      
+      duration_in_slices = 12
+
+      
+      while  # if the track has too few samples, randomly pick another track, until a suitable track is found
+        track_id = subset_of_track_ids.shuffle.first
+        self.track = Track.find(track_id.to_s)
+        track_onset_array = self.track.onset_times
+        break if (track_onset_array.size > (duration_in_slices + 1))
+      end
+
+      max_start_index = track_onset_array.size - duration_in_slices # figure out the max starting index that won't go out of bounds
+      self.start_onset_index = rand(0...max_start_index) # pick a random starting index
+      self.stop_onset_index = [(self.start_onset_index + duration_in_slices), (track_onset_array.size - 1)].min
+
+      self.start_onset_time = self.track.onset_times[self.start_onset_index]
+      self.stop_onset_time = self.track.onset_times[self.stop_onset_index]
+      debugger if self.stop_onset_time.to_i < 1
+
+      shift_slice_forward_one_index = {
+        start_onset_index: start_onset_index+1,
+        stop_onset_index: stop_onset_index+1,
+        start: sec_dot_milli_to_milli(self.track.onset_times[self.start_onset_index+1]), 
+        duration: ((sec_dot_milli_to_milli(self.track.onset_times[self.stop_onset_index+1]) - sec_dot_milli_to_milli(self.track.onset_times[self.start_onset_index+1]) ).round(5) ) ,
+        url: "get http://localhost:3000/get_slice/#{self.track_id}/#{start_onset_index+1}/#{stop_onset_index}",
+      }
+
+      shift_slice_backward_one_index = {
+        start_onset_index: start_onset_index-1,
+        stop_onset_index: stop_onset_index-1,
+        start: sec_dot_milli_to_milli(self.track.onset_times[self.start_onset_index-1]), 
+        duration: ((sec_dot_milli_to_milli(self.track.onset_times[self.stop_onset_index-1]) - sec_dot_milli_to_milli(self.track.onset_times[self.start_onset_index-1]) ).round(5) ) ,
+        url: "",
+
+      }
+
+
+
+
+
+      self.save 
+
+      # debugger #if self.duration < 0
+      return {
+        track_id: self.track.id.to_s,
+        track_path: self.track.escaped_path_and_file,
+        start_onset_index: start_onset_index,
+        stop_onset_index: stop_onset_index,
+        start: sec_dot_milli_to_milli(self.start_onset_time), 
+        duration: (self.duration ) ,
+        shift_slice_forward_one_index: shift_slice_forward_one_index,
+        shift_slice_backward_one_index: shift_slice_backward_one_index,
+
+      }
+    end
+
+
+
+
     def randomize_patch(subset_of_track_ids)
 
       return if self.is_frozen?
@@ -58,7 +120,7 @@
 
 
     def duration
-      duration = sec_dot_milli_to_milli(stop_onset_time) - sec_dot_milli_to_milli(start_onset_time) 
+      duration = (sec_dot_milli_to_milli(self.stop_onset_time) - sec_dot_milli_to_milli(self.start_onset_time) ).round(5)
       return duration
     end
 
@@ -131,7 +193,7 @@
 
     def cut_sample(pad)
       valid_sample?
-      pad_name = "pad_#{pad}"
+      pad_name = "pad_#{0}"
       puts "OKOK"
       puts self.patch_index
       puts "OKOK"
